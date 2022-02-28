f9957312d02f47fc2cab84f156e332aa27fe1c2c
DandyChux <chuck@justlearn.io>
2022-02-27 23:15:25 -0500

    FEAT: add PHP function to return full name

--- default.php
+++ default.php
@@ -1,6 +1,15 @@
-<?php
-function writeMsg() {
-    echo 'Hello World!';
-}
-writeMsg();
-?>
+<html>
+
+<body>
+    <?php
+    function fullName($fname, $year) {
+        echo "$fname Okoroji. Born in $year <br>";
+    }
+
+    fullName("Chuck", "2002");
+    fullName("Nnadozie", "2004");
+    fullName("Chidiebere", "2010");
+    fullName("Chinyere", "2013");
+    ?>
+</body>
+</html>
